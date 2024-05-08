6af81a4b34c5fd6bc1d515a3b02d3a4d48652fdc
Yantong_zhang <1678532032@qq.com>
2024-05-08 17:49:38 +0800

    feat: density-independent pixels (dp) & px(pixel) 的转化规则

--- my-widgets/src/main/java/com/zyt/widgets/utils/ViewKtx.kt
+++ my-widgets/src/main/java/com/zyt/widgets/utils/ViewKtx.kt
@@ -2,10 +2,23 @@ package com.zyt.widgets.utils
 
 import android.content.res.Resources
 
+/**
+ * px: 像素
+ * dp(device independent pixels): 与设备像素密度无关的单位
+ *
+ * dp * density = px
+ * dp * (densityDpi / 160) = px
+ */
 val Int.dp: Float
     get() {
         val scale = Resources.getSystem().displayMetrics.density
-        return this * scale
+        return (this * scale + 0.5f)
+    }
+
+val Int.px: Float
+    get() {
+        val scale = Resources.getSystem().displayMetrics.density
+        return (this / scale + 0.5f)
     }
 
 
