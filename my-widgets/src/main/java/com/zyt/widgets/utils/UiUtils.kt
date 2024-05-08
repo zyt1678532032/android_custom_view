bb33e2fefe1bb225a87225d6c60cf6b8d3ce223c
Yantong_zhang <1678532032@qq.com>
2024-05-08 19:52:38 +0800

    feat: px2dp

--- my-widgets/src/main/java/com/zyt/widgets/utils/UiUtils.kt
+++ my-widgets/src/main/java/com/zyt/widgets/utils/UiUtils.kt
@@ -6,8 +6,13 @@ object UiUtils {
 
     @JvmStatic
     fun dp2px(context: Context, dpValue: Float): Int {
-        val scale = context.resources
-            .displayMetrics.density
+        val scale = context.resources.displayMetrics.density
         return (dpValue * scale + 0.5f).toInt()
+    }
+
+    @JvmStatic
+    fun px2dp(context: Context, pxValue: Float): Int {
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f).toInt()
     }
 }
